43f55c64c89f42ef162806c8abf838d4422bef38
josiah34 <josiah_g@live.com>
2022-09-01 11:02:04 -0400

    Added Main

--- Strings/src/Main.java
+++ Strings/src/Main.java
@@ -1,4 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        String myString = "This is myString";
+        System.out.println(myString);
+        myString = "This is myString but a little bit different than the original\n";
+        System.out.println(myString);
+
     }
 }
